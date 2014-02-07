9ffe3de3cb796791cc2de7c88ca81751163f4f28
Alan Files <alangfiles@gmail.com>
2014-02-06 22:34:04 -0500

    moving data to json

---
+++ js/alanData.js
@@ -0,0 +1,121 @@
+var alanData = {
+	"runs": [
+		{
+			"timeFormatted": "15:04",
+			"timeMathed": 15+04/60,
+			"date": Date.UTC(2013, 9, 15, 17, 49)
+		},
+		{
+			"timeFormatted": "15:01",
+			"timeMathed": 15+01/60,
+			"date": Date.UTC(2013, 10, 0, 13, 00)
+		},
+		{
+			"timeFormatted": "14:39",
+			"timeMathed": 14+39/60,
+			"date": Date.UTC(2013, 11, 12, 17, 05)
+		},
+		{
+			"timeFormatted": "15:23",
+			"timeMathed": 15+23/60,
+			"date": Date.UTC(2013, 11, 19, 17, 19)
+		},
+		{
+			"timeFormatted": "15:38",
+			"timeMathed": 15+38/60,
+			"date": Date.UTC(2014, 0, 4, 10, 14)
+		},
+		{
+			"timeFormatted": "14:20",
+			"timeMathed": 14+20/60,
+			"date": Date.UTC(2014, 0, 10, 16, 59)
+		},
+		{
+			"timeFormatted": "14:53",
+			"timeMathed": 14+53/60,
+			"date": Date.UTC(2014, 0, 15, 16, 40)
+		},
+		{
+			"timeFormatted": "17:15",
+			"timeMathed": 17+15/60,
+			"date": Date.UTC(2014, 0, 18, 17, 33),
+			"note": "With Noah"
+		},
+		{
+			"timeFormatted": "17:39",
+			"timeMathed": 17+39/60,
+			"date": Date.UTC(2014, 0, 25, 17, 39),
+			"note": "snowy!"
+		},
+		{
+			"timeFormatted": "15:40",
+			"timeMathed": 15+40/60,
+			"date": Date.UTC(2014, 1, 4, 17, 48),
+			"note": "snowy!"
+		},
+		// {
+		// 	"timeFormatted": "15:40",
+		// 	"timeMathed": 15+40/60,
+		// 	"date": Date.UTC(2014, 1, 4, 17, 48),
+		// 	"note": "snowy!"
+		// }
+	],
+	"weights": [
+	    {"pounds": 178.2, "Time": "", "date": "1/28/2013"},
+	    {"pounds": 171.4, "Time": "", "date": "2/2/2013"},
+	    {"pounds": 171.0, "Time": "", "date": "2/10/2013"},
+	    {"pounds": 170.4, "Time": "", "date": "2/17/2013"},
+	    {"pounds": 175.2, "Time": "", "date": "2/24/2013"},
+	    {"pounds": 169.6, "Time": "", "date": "3/6/2013"},
+	    {"pounds": 172.8, "Time": "", "date": "6/6/2013"},
+	    {"pounds": 172.6, "Time": "", "date": "6/7/2013"},
+	    {"pounds": 173.8, "Time": "", "date": "6/8/2013"},
+	    {"pounds": 172.6, "Time": "", "date": "6/9/2013"},
+	    {"pounds": 172.6, "Time": "", "date": "6/10/2013"},
+	    {"pounds": 170.8, "Time": "", "date": "6/11/2013"},
+	    {"pounds": 173.6, "Time": "", "date": "6/12/2013"},
+	    {"pounds": 171.2, "Time": "", "date": "6/13/2013"},
+	    {"pounds": 173.4, "Time": "", "date": "6/14/2013"},
+	    {"pounds": 171.4, "Time": "", "date": "6/18/2013"},
+	    {"pounds": 171.8, "Time": "", "date": "6/25/2013"},
+	    {"pounds": 170.2, "Time": "7:58 AM", "date": "7/2/2013"},
+	    {"pounds": 174.4, "Time": "5:57 PM", "date": "7/5/2013"},
+	    {"pounds": 171.6, "Time": "5:36 PM", "date": "7/7/2013"},
+	    {"pounds": 173.6, "Time": "6:52 PM", "date": "7/8/2013"},
+	    {"pounds": 173.4, "Time": "7:46 AM", "date": "7/10/2013"},
+	    {"pounds": 172.2, "Time": "7:44 AM", "date": "7/11/2013"},
+	    {"pounds": 172.8, "Time": "7:56 AM", "date": "7/15/2013"},
+	    {"pounds": 173.6, "Time": "7:54 AM", "date": "7/16/2013"},
+	    {"pounds": 172.2, "Time": "7:52 AM", "date": "7/17/2013"},
+	    {"pounds": 172.4, "Time": "8:18 AM", "date": "7/18/2013"},
+	    {"pounds": 172.2, "Time": "12:29 PM", "date": "7/20/2013"},
+	    {"pounds": 172.4, "Time": "7:09 AM", "date": "7/22/2013"},
+	    {"pounds": 172.6, "Time": "7:49 AM", "date": "7/23/2013"},
+	    {"pounds": 173.4, "Time": "4:28 PM", "date": "7/29/2013"},
+	    {"pounds": 171.8, "Time": "9:18 AM", "date": "7/31/2013"},
+	    {"pounds": 174.4, "Time": "9:10 PM", "date": "8/11/2013"},
+	    {"pounds": 171.8, "Time": "7:49 AM", "date": "8/13/2013"},
+	    {"pounds": 171.2, "Time": "7:49 AM", "date": "8/15/2013"},
+	    {"pounds": 174.4, "Time": "10:00 PM", "date": "8/17/2013"},
+	    {"pounds": 171.6, "Time": "8:42 AM", "date": "8/18/2013"},
+	    {"pounds": 170.8, "Time": "7:30 AM", "date": "8/19/2013"},
+	    {"pounds": 172.6, "Time": "7:14 AM", "date": "8/21/2013"},
+	    {"pounds": 171.8, "Time": "", "date": "9/1/2013"},
+	    {"pounds": 175.4, "Time": "", "date": "9/3/2013"},
+	    {"pounds": 172.8, "Time": "7:52 AM", "date": "9/4/2013"},
+	    {"pounds": 174.2, "Time": "4:36 PM", "date": "9/5/2013"},
+	    {"pounds": 172.0, "Time": "7:21 PM", "date": "10/15/2013"},
+	    {"pounds": 174.2, "Time": "8:52 AM", "date": "10/25/2013"},
+	    {"pounds": 176.8, "Time": "10:00 PM", "date": "10/27/2013"},
+	    {"pounds": 175.6, "Time": "10:43 PM", "date": "11/11/2013"},
+	    {"pounds": 179.4, "Time": "1:39 PM", "date": "1/1/2014"},
+	    {"pounds": 176.4, "Time": "10:14 AM", "date": "1/4/2014"},
+	    {"pounds": 176.0, "Time": "8:50 AM", "date": "1/8/2014"},	
+	    {"pounds": 176.6, "Time": "5:21 PM", "date": "1/10/2014"},	
+	    {"pounds": 175.2, "Time": "8:40 AM", "date": "1/14/2014"},	
+	    {"pounds": 175.0, "Time": "8:19 AM", "date": "1/24/2014"},	
+	    {"pounds": 177.6, "Time": "08:53 AM", "date": "1/30/2014"},
+	    {"pounds": 177.4, "Time": "5:48 PM", "date": "2/4/2014"},	
+	    // {"pounds": , "Time": "", "date": ""}
+  ]
+}
