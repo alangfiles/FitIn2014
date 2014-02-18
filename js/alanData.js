43d3bf72dde1fe91af45accad453f40003257429
Alan Files <alangfiles@gmail.com>
2014-02-18 16:46:29 -0500

    Update alanData.js

--- js/alanData.js
+++ js/alanData.js
@@ -63,6 +63,11 @@ var alanData = {
 			"timeMathed": 16+1/60,
 			"date": Date.UTC(2014, 1, 13, 17, 45)
 		},
+		{
+			"timeFormatted": "14:54",
+			"timeMathed": 14+54/60,
+			"date": Date.UTC(2014, 1, 18, 16, 30)
+		},
 		// {
 		// 	"timeFormatted": "15:40",
 		// 	"timeMathed": 15+40/60,
