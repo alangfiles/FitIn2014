9d1ec8ce48ee678d25e4b34f81447b81e7166e68
Alan Files <alangfiles@gmail.com>
2014-06-24 07:38:58 -0400

    Update alanData.js

--- js/alanData.js
+++ js/alanData.js
@@ -448,6 +448,8 @@ var alanData = {
 	    {"pounds": 177.2, "Time": "8:00 AM", "date": "6/20/2014"},
 	    {"pounds": 176.8, "Time": "8:00 AM", "date": "6/21/2014"},
 	    {"pounds": 175.8, "Time": "8:00 AM", "date": "6/22/2014"},
+	    {"pounds": 177.2, "Time": "8:00 AM", "date": "6/23/2014"},
+	    {"pounds": 175.0, "Time": "8:00 AM", "date": "6/24/2014"},
 	    // {"pounds": , "Time": "", "date": ""}
   ]
 }
